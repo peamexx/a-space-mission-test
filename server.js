b5d35daac879f487d8a4626f7b0cd3ebed432f86
peamexx <peamexx@gmail.com>
2022-06-28 13:30:57 +0900

    db 연결

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +10,14 @@ app.use(express.static('src'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/src/index.html');
 });
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('mongoDB has connected...')
+    }
+})
 
 app.listen(port, function() {
     console.log('listening on...')
