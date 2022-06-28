d335887f0012907e07360bc41ce46c44084e5f38
peamexx <peamexx@gmail.com>
2022-06-28 18:09:07 +0900

    js 시작

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+var ASM = ASM || {};
+
+(function() {
+    if(typeof exports !== 'undefined') {
+        ASM = exports;
+    } else {
+        ASM = this.ASM = {};
+    }
+
+    ASM.deck = {};
+    ASM.version = '0.0.1';
+})();
