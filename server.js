b1dc2ab92e8f4238840c096b459fee5e8cde0224
= <=>
2022-06-28 09:05:48 +0900

    port 변수로 변경

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', function(req, res) {
     res.send('안녕하세요')
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log('listening on...')
 });
