7e128b5b98bc6de0174b72e80061095f2acb3b67
peamexx <peamexx@gmail.com>
2022-06-29 10:50:14 +0900

    db주소/스키마 변경

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const User = new Schema({
-    userName: {
+const user = new Schema({
+    id: {
         type: String,
         required: true
     },
@@ -12,4 +12,4 @@ const User = new Schema({
     }
 });
 
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('users', user);
